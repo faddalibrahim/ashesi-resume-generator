8530689c766e4301afa1fd2262437716a77e4ea8
Faddal Ibrahim <faddalibrahim@gmail.com>
2021-09-10 20:15:01 +0000

    getting started

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import appStyles from "css/app.module.css";
+import { resume_default_data } from "./utils/resume_default_data";
+
+function App() {
+  return <div className={appStyles.app}>lets get started</div>;
+}
+
+export default App;
---
+++ src/utils/resume_default_data.js
@@ -0,0 +1,102 @@
+export const resume_default_data = {
+  "PERSONAL INFORMATION": {
+    name: "Faddal Ibrahim",
+    "postal address": "P.O. Box 19331 Accra North AN",
+    phone_number: "0557440501",
+    nationality: "Ghanaian",
+    emails: ["faddalibrahim@gmail.com", "faddal.ibrahim@ashesi.edu.gh"],
+    links: ["github", "linkedin"],
+  },
+  EDUCATION: {
+    university: {
+      name: "Ashesi University College",
+      location: "Berekusu, Eastern Region",
+      program: "BSc. Computer Science",
+      graduation: "July 2023",
+      "Cumulative GPA": "3.75/4.00",
+    },
+    "high school": {
+      name: "ABCD Senior High School",
+      location: "Cantonments Accra",
+      program: "*General Arts (WASSCE)",
+      duration: "Sept 2009 – July 2012",
+    },
+  },
+  "ACHIEVEMENTS/AWARDS": [
+    {
+      accolade: "The MasterCard Foundation Scholar Program Scholarship",
+      duration: "2012 - 2016",
+      issuer: "Ashesi University College",
+    },
+    {
+      accolade: "*Deans List",
+      duration: "2012 - 2013",
+      issuer: "Ashesi University College",
+    },
+  ],
+  "WORK EXPERIENCE": [
+    {
+      institution: "ABC Children’s Home",
+      location: "Cantonments, Accra",
+      duration: "Nov – Dec 2013",
+      role: "Marketing Intern",
+      impact: [
+        "Revamped the Annual Company Street Festival",
+        "Jump-started a charity project and raised $3000 to help provide shelter for street children",
+        "Updated the company’s website and increased website traffic by 20%",
+      ],
+    },
+    {
+      institution: "XYZ Foundation",
+      location: "Dzorwulu, Accra",
+      duration: "Dec 2012 – May 2013",
+      role: "President",
+      impact: [
+        "Managed club and presided over club activities and Editorial Meetings",
+        "Organized seminars involving key figures to dialogue on the role of leadership in Africa",
+        "Increased membership and developed club programs by 22% and 40% respectively",
+      ],
+    },
+  ],
+  "PROJECTS AND RESEARCH": [
+    {
+      institution: "XYZ Foundation",
+      role: "President",
+      impact: [
+        "Assisted a global team in designing and implementing a business model to provide digital access to 5.5 billion people living on less than $10 per day using a model that offers free mobile phone service",
+      ],
+    },
+  ],
+  "CO-CURRICULAR ACTIVITIES": [
+    {
+      institution: "Ashesi University College",
+      activity: "Ashesi Robotics Experience",
+      duration: "Nov – Dec 2013",
+      role: "Mentor",
+      impact: [
+        "Mentored 5 high school students for 1 week and helped them design a Robotic Tour Guide",
+        "Supervised and evaluated group projects and weekly tasks",
+      ],
+    },
+    {
+      institution: "Ashesi University College",
+      activity: "Berekusu Math Community Project",
+      duration: "Jan – Dec 2012",
+      role: "Mentor",
+      impact: [
+        "Tutored students in Berekuso to help them improve upon their math skills",
+        "Organized a weekly math quiz for the students and awarded the winning team with books and stationary",
+      ],
+    },
+  ],
+  SKILLS: [
+    "Fluent in French and English",
+    "Programming Languages (Proficient in Java, C++, SQL, HTML, CSS, PHP, and JavaScript)",
+    "Graphic designing using software such as Indigo Studio, Balsamic, Pencil, Lumzy",
+  ],
+  REFERENCES: "Available upon request",
+  MISCILLANEOUS: [
+    "*WASSCE - West African Senior School Certificate Examination",
+    "*Dean's List - Awarded to students with GPA of 3.5 and above",
+  ],
+};
